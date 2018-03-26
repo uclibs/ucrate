073218b3acbd3d3d4909282ab744126930be9069
Glen Horton <hortongn@ucmail.uc.edu>
2018-03-26 15:51:07 -0400

    Set sandbox URL

--- config/initializers/ucrate.rb
+++ config/initializers/ucrate.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
-Rails.application.config.application_root_url = 'http://localhost:3000'
+Rails.application.config.application_root_url = 'http://curly.libraries.uc.edu:3002'
 Rails.application.config.rss_feed_rows = 10
