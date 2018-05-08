771438745c577adbd993f83986b416fce37eaa89
Olivia Chang <changy6@mail.uc.edu>
2018-05-08 12:26:05 -0400

    deleted the languages

--- app/helpers/hyrax_helper.rb
+++ app/helpers/hyrax_helper.rb
@@ -2,4 +2,14 @@ module HyraxHelper
   include ::BlacklightHelper
   include Hyrax::BlacklightOverride
   include Hyrax::HyraxHelperBehavior
+
+  # Which translations are available for the user to select
+  # @return [Hash<String,String>] locale abbreviations as keys and flags as values
+  def available_translations
+    {
+      'en' => 'English',
+      'es' => 'Español',
+      'zh' => '中文'
+    }
+  end
 end
