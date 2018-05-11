ce3eb1a5f9b19d533d046955f4323caaabfa7300
Glen Horton <hortongn@ucmail.uc.edu>
2018-05-11 18:51:36 +0000

    Update to Hyrax v2.1.0.rc2

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -95,6 +95,7 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include FactoryBot::Syntax::Methods
+  config.include OptionalExample
 
   config.include Devise::Test::IntegrationHelpers, type: :feature
   config.include Devise::Test::ControllerHelpers, type: :controller
