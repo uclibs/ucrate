c59dbc5ec5e41563c3b4375984b8d2bf71ee9525
Jayanth Dungavath <jayanthdeejay@gmail.com>
2018-05-15 20:36:46 -0400

    Added bixby gem with bundle update

--- spec/models/hyrax/uploaded_file_spec.rb
+++ spec/models/hyrax/uploaded_file_spec.rb
@@ -5,9 +5,8 @@ require 'rails_helper'
 RSpec.describe Hyrax::UploadedFile, type: :model do
   context 'when an uploaded file has a virus'
 
-  let(:file) { fixture_file_upload('/world.png') }
-
   subject(:create_with_virus) { described_class.create(file: file) }
+  let(:file) { fixture_file_upload('/world.png') }
 
   before do
     allow(Hydra::Works::VirusCheckerService).to receive(:file_has_virus?).and_return(true)
