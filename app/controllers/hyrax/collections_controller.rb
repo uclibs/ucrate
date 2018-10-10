a5b0018872a992e5c4a7b428b92b1a67394b63c3
bsp3ars <bsp3ars@gmail.com>
2018-10-10 16:32:42 -0400

    Upgrade hyrax to 2.3.3

--- app/controllers/hyrax/collections_controller.rb
+++ app/controllers/hyrax/collections_controller.rb
@@ -4,6 +4,7 @@ require Hyrax::Engine.root.join('app/controllers/hyrax/collections_controller.rb
 module Hyrax
   class CollectionsController < ApplicationController
     def show
+      @curation_concern ||= ActiveFedora::Base.find(params[:id])
       presenter
       query_collection_members
       @permalinks_presenter = PermalinksPresenter.new(collection_path(locale: nil))
