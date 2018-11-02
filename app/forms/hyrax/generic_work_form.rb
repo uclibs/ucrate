40b628332bc7829ecf16fe7ce1a806f4e4e8c480
Olivia Chang <changy6@mail.uc.edu>
2018-11-02 11:51:06 -0400

    fix description field doesnt persist on generic work

--- app/forms/hyrax/generic_work_form.rb
+++ app/forms/hyrax/generic_work_form.rb
@@ -17,7 +17,7 @@ module Hyrax
     self.terms += %i[college department]
 
     ## Removing terms that we don't use
-    self.terms -= %i[rights_statement keyword source contributor description based_near identifier resource_type]
+    self.terms -= %i[rights_statement keyword source contributor based_near identifier resource_type]
 
     ## Setting custom required fields
     self.required_fields = %i[title creator college department description license]
