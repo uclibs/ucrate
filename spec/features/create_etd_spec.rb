cb34415a0de6d406133b91060e54515123e99c9e
crowesn <crowesn@gmail.com>
2018-11-16 11:31:24 -0500

    expand work creation feature specs

--- spec/features/create_etd_spec.rb
+++ spec/features/create_etd_spec.rb
@@ -6,8 +6,8 @@ require 'rails_helper'
 include Warden::Test::Helpers
 
 # NOTE: If you generated more than one work, you have to set "js: true"
-RSpec.describe 'Create a Etd', js: true do
-  context 'a logged in user' do
+RSpec.describe 'Create a Etd', :feature, js: true do
+  context 'a logged in user', :clean_repo do
     let(:user_attributes) do
       { email: 'test@example.com' }
     end
@@ -17,6 +17,28 @@ RSpec.describe 'Create a Etd', js: true do
     let(:admin_set_id) { AdminSet.find_or_create_default_admin_set_id }
     let(:permission_template) { Hyrax::PermissionTemplate.find_or_create_by!(source_id: admin_set_id) }
     let(:workflow) { Sipity::Workflow.create!(active: true, name: 'test-workflow', permission_template: permission_template) }
+
+    # Form values
+    let(:title) { 'My Test Work' }
+    let(:college) { 'Business' }
+    let(:creator) { 'Doe, Jane' }
+    let(:abstract) { 'Description' }
+    let(:advisor) { 'Neil Armstrong' }
+    let(:committee_member) { 'John Glenn' }
+    let(:degree) { 'M.S.' }
+    let(:degree_date) { '2018' }
+    let(:type) { 'Painting' }
+    let(:program) { 'My University Department' }
+    let(:license) { 'Attribution-ShareAlike 4.0 International' }
+    let(:publisher) { 'Spy Magazine' }
+    let(:alternate_title) { 'Or, your test work' }
+    let(:subject_term) { 'Isnt everything about death in one way or another' }
+    let(:geographic_subject) { 'Cincinnati' }
+    let(:time_period) { 'Year of the whopper' }
+    let(:language) { 'American English' }
+    let(:required_software) { 'Corel draw' }
+    let(:note) { 'This is a note' }
+    let(:external_link) { 'http://www.chitterchat.com' }
 
     before do
       # Create a single action that can be taken
@@ -60,37 +82,73 @@ RSpec.describe 'Create a Etd', js: true do
       end
       click_link "Metadata" # switch tab
 
+      # Required fields
+
       title_element = find_by_id("etd_title")
-      title_element.set("My Test Work  ") # Add whitespace to test it getting removed
+      title_element.set(title) # Add whitespace to test it getting removed
 
       college_element = find_by_id("etd_college")
-      college_element.select("Business")
+      college_element.select(college)
 
       expect(page).to have_content("License Wizard")
       expect(page).not_to have_content('Rights statement')
-      select 'Attribution-ShareAlike 4.0 International', from: 'etd_license'
+      select license, from: 'etd_license'
 
-      fill_in('Creator', with: 'Doe, Jane')
-      fill_in('Abstract', with: 'Description')
-      fill_in('Advisor', with: 'Ima Advisor')
-      fill_in('Degree Program', with: 'Test Department')
-      expect(page).to have_selector("select[id='etd_college'][autocomplete='on']")
-      expect(page).to have_selector("input[id='etd_department'][autocomplete='on']")
-      expect(page).to have_selector("input[id='etd_advisor'][autocomplete='on']")
-      expect(page).to have_selector("input[id='etd_publisher'][autocomplete='on']")
-      expect(page).to have_selector("input[id='etd_subject'][autocomplete='on']")
-      expect(page).to have_selector("input[id='etd_geo_subject'][autocomplete='on']")
-      expect(page).to have_selector("input[id='etd_time_period'][autocomplete='on']")
-      expect(page).to have_selector("input[id='etd_language'][autocomplete='on']")
+      fill_in('Creator', with: creator)
+      fill_in('Abstract', with: abstract)
+      fill_in('Advisor', with: advisor)
+      fill_in('Degree Program', with: program)
+
+      # Non-required fields
+
+      fill_in('Committee Member', with: committee_member)
+      fill_in('Degree', with: degree)
+      fill_in('Degree Date', with: degree_date)
+      fill_in('Publisher (Required for DOI registration)', with: publisher)
+      fill_in('etd_alternate_title', with: alternate_title)
+      select(type, from: 'etd_genre')
+      fill_in('Subject', with: subject_term)
+      fill_in('Geographic Subject', with: geographic_subject)
+      fill_in('Time Period', with: time_period)
+      fill_in('Language', with: language)
+      fill_in('Required Software', with: required_software)
+      fill_in('Note', with: note)
+      fill_in('External Link', with: external_link)
 
       choose('etd_visibility_open')
       expect(page).not_to have_content('Please note, making something visible to the world (i.e. marking this as Open Access) may be viewed as publishing which could impact your ability to')
       check('agreement')
 
       click_on('Save')
-      expect(page).to have_content('My Test Work')
+      expect(page).to have_content(title)
       expect(page).to have_content "Your files are being processed by Scholar@UC in the background."
       expect(page).to have_content("Permanent link to this page")
+
+      # Edit the work to verify form values persist
+      click_on('Edit')
+
+      expect(page).to have_field('etd_title', with: 'My Test Work')
+      expect(page).to have_field('etd_creator', with: creator)
+      expect(page).to have_select('etd_college', selected: college)
+      expect(page).to have_field('Degree Program', with: program)
+      expect(page).to have_field('Committee Member', with: committee_member)
+      expect(page).to have_field('Degree', with: degree)
+      expect(page).to have_field('Degree Date', with: degree_date)
+      expect(page).to have_field('Abstract', with: abstract)
+      expect(page).to have_select('etd_license', selected: license)
+      expect(page).to have_field('Publisher (Required for DOI registration)', with: publisher)
+      expect(page).to have_field('etd_alternate_title', with: alternate_title)
+      # expect(page).to have_select('etd_genre', selected: type) # This is broken see https://github.com/uclibs/ucrate/issues/514
+      expect(page).to have_field('Subject', with: subject_term)
+      expect(page).to have_field('Geographic Subject', with: geographic_subject)
+      expect(page).to have_field('Time Period', with: time_period)
+      expect(page).to have_field('Language', with: language)
+      expect(page).to have_field('Required Software', with: required_software)
+      expect(page).to have_field('Note', with: note)
+      expect(page).to have_field('External Link', with: external_link)
+
+      check('agreement')
+      click_on('Save')
 
       click_on('image.jp2')
       expect(page).to have_content("Permanent link to this page")
--- spec/features/create_student_work_spec.rb
+++ spec/features/create_student_work_spec.rb
@@ -6,8 +6,8 @@ require 'rails_helper'
 include Warden::Test::Helpers
 
 # NOTE: If you generated more than one work, you have to set "js: true"
-RSpec.describe 'Create a StudentWork', js: true do
-  context 'a logged in user' do
+RSpec.describe 'Create a StudentWork', :feature, js: true do
+  context 'a logged in user', :clean_repo do
     let(:user_attributes) do
       { email: 'test@example.com' }
     end
@@ -17,6 +17,28 @@ RSpec.describe 'Create a StudentWork', js: true do
     let(:admin_set_id) { AdminSet.find_or_create_default_admin_set_id }
     let(:permission_template) { Hyrax::PermissionTemplate.find_or_create_by!(source_id: admin_set_id) }
     let(:workflow) { Sipity::Workflow.create!(active: true, name: 'test-workflow', permission_template: permission_template) }
+
+    # Form values
+    let(:title) { 'My Test Work' }
+    let(:college) { 'Business' }
+    let(:creator) { 'Doe, Jane' }
+    let(:description) { 'Description' }
+    let(:advisor) { 'Neil Armstrong' }
+    let(:committee_member) { 'John Glenn' }
+    let(:degree) { 'M.S.' }
+    let(:date_created) { '2018' }
+    let(:type) { 'Painting' }
+    let(:program) { 'My University Department' }
+    let(:license) { 'Attribution-ShareAlike 4.0 International' }
+    let(:publisher) { 'Spy Magazine' }
+    let(:alternate_title) { 'Or, your test work' }
+    let(:subject_term) { 'Isnt everything about death in one way or another' }
+    let(:geographic_subject) { 'Cincinnati' }
+    let(:time_period) { 'Year of the whopper' }
+    let(:language) { 'American English' }
+    let(:required_software) { 'Corel draw' }
+    let(:note) { 'This is a note' }
+    let(:external_link) { 'http://www.chitterchat.com' }
 
     before do
       # Create a single action that can be taken
@@ -57,22 +79,39 @@ RSpec.describe 'Create a StudentWork', js: true do
       end
       click_link "Metadata" # switch tab
 
+      # Required fields
+
       title_element = find_by_id("student_work_title")
-      title_element.set("My Test Work  ") # Add whitespace to test it getting removed
+      title_element.set(title)
 
       expect(page).to have_content("License Wizard")
       expect(page).not_to have_content('Rights statement')
-      select 'Attribution-ShareAlike 4.0 International', from: 'student_work_license'
+      select license, from: 'student_work_license'
 
       college_element = find_by_id("student_work_college")
-      college_element.select("Business")
+      college_element.select(college)
 
-      fill_in('student_work_title', with: 'My Test Work')
-      fill_in('Creator', with: 'Doe, Jane')
-      fill_in('Description', with: 'testing')
-      fill_in('Advisor', with: 'Lamb, Mary')
-      fill_in('Department', with: 'Department')
-      fill_in('Degree', with: 'Degree')
+      fill_in('student_work_title', with: title)
+      fill_in('Creator', with: creator)
+      fill_in('Description', with: description)
+      fill_in('Advisor', with: advisor)
+      fill_in('Program or Department', with: program)
+      fill_in('Degree', with: degree)
+
+      # Non-required fields
+
+      fill_in('Degree', with: degree)
+      fill_in('Date Created', with: date_created)
+      fill_in('Publisher (Required for DOI registration)', with: publisher)
+      fill_in('student_work_alternate_title', with: alternate_title)
+      select(type, from: 'student_work_genre')
+      fill_in('Subject', with: subject_term)
+      fill_in('Geographic Subject', with: geographic_subject)
+      fill_in('Time Period', with: time_period)
+      fill_in('Language', with: language)
+      fill_in('student_work_required_software', with: required_software)
+      fill_in('Note', with: note)
+      fill_in('External Link', with: external_link)
 
       choose('student_work_visibility_open')
       expect(page).not_to have_content('Please note, making something visible to the world (i.e. marking this as Open Access) may be viewed as publishing which could impact your ability to')
@@ -82,6 +121,31 @@ RSpec.describe 'Create a StudentWork', js: true do
       expect(page).to have_content('My Test Work')
       expect(page).to have_content "Your files are being processed by Scholar@UC in the background."
       expect(page).to have_content("Permanent link to this page")
+
+      # Edit the work to verify form values persist
+      click_on('Edit')
+
+      expect(page).to have_field('student_work_title', with: 'My Test Work')
+      expect(page).to have_field('student_work_creator', with: creator)
+      expect(page).to have_select('student_work_college', selected: college)
+      expect(page).to have_field('Program or Department', with: program)
+      expect(page).to have_field('Degree', with: degree)
+      expect(page).to have_field('Date Created', with: date_created)
+      expect(page).to have_field('Description', with: description)
+      expect(page).to have_select('student_work_license', selected: license)
+      expect(page).to have_field('Publisher (Required for DOI registration)', with: publisher)
+      expect(page).to have_field('student_work_alternate_title', with: alternate_title)
+      expect(page).to have_select('student_work_genre', selected: type)
+      expect(page).to have_field('Subject', with: subject_term)
+      expect(page).to have_field('Geographic Subject', with: geographic_subject)
+      expect(page).to have_field('Time Period', with: time_period)
+      expect(page).to have_field('Language', with: language)
+      expect(page).to have_field('student_work_required_software', with: required_software)
+      expect(page).to have_field('Note', with: note)
+      expect(page).to have_field('External Link', with: external_link)
+
+      check('agreement')
+      click_on('Save')
 
       click_on('image.jp2')
       expect(page).to have_content("Permanent link to this page")
