51868bb74a5b7be6684ec2877d3304b59b452b4f
Brandon Spears <bsp3ars@gmail.com>
2019-01-23 13:21:55 -0500

    Use eager_load_paths

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module ScholarUc
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.exceptions_app = routes
-    config.autoload_paths << Rails.root.join('lib')
+    config.eager_load_paths << Rails.root.join('lib')
     # REMOVE_ME: Temporarily remove strong params
     # config.action_controller.permit_all_parameters = true
   end
