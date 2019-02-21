f402677bfbe5a368ba5fe57efdbcfe1d7274fc68
Chuck Greenman <charlesgreenman@gmail.com>
2019-02-21 09:17:01 -0500

    Adds test for analytics configuration override.

---
+++ spec/services/hyrax/analytics_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+require 'rails_helper'
+RSpec.describe Hyrax::Analytics::Config do
+  subject { described_class }
+  let!(:response) { described_class.load_from_yaml }
+  it 'defines analytics variables' do
+    expect(response.app_name).to eq("GOOGLE_OAUTH_APP_NAME")
+    expect(response.app_version).to eq("GOOGLE_OAUTH_APP_VERSION")
+    expect(response.privkey_path).to eq("analytics_privkey_path")
+    expect(response.privkey_secret).to eq("analytics_privkey_secret")
+    expect(response.client_email).to eq("analytics_client_email")
+  end
+end
