8fbee53ef67f81088ea3b625d7ab830a5d119165
crowesn <crowesn@gmail.com>
2019-05-01 15:42:03 -0400

    replace chromedriver with webdrivers for capybara tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,6 +32,10 @@ Shoulda::Matchers.configure do |config|
   end
 end
 
+# pin chromedriver version so capybara tests pass
+
+Webdrivers::Chromedriver.version = '72.0.3626.69'
+
 unless ENV['SKIP_MALEFICENT']
   # See https://github.com/jeremyf/capybara-maleficent
   # Wrap Capybara matchers with sleep intervals to reduce fragility of specs.
