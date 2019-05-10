472637bad5121297d49cdb6840addd778893ced9
Chuck Greenman <charlesgreenman@gmail.com>
2019-05-10 11:32:12 -0400

    Delete draft dois.

--- app/jobs/identifier_delete_job.rb
+++ app/jobs/identifier_delete_job.rb
@@ -2,10 +2,20 @@
 
 class IdentifierDeleteJob < ActiveJob::Base
   def perform(identifier_uri)
-    RestClient.put(post_uri(identifier_uri), "{\n  \"data\": {\n    \"type\": \"dois\",\n    \"attributes\": {\n      \"event\": \"hide\",\n   \"url\": \"https://datacite.org/invalid.html\"\n    }\n  }\n}", content_type: :json)
+    state = get_state(identifier_uri)
+
+    if state == "draft"
+      datacite_resource(identifier_uri).delete
+    else
+      datacite_resource(identifier_uri).put("{\n  \"data\": {\n    \"type\": \"dois\",\n    \"attributes\": {\n      \"event\": \"hide\",\n   \"url\": \"https://datacite.org/invalid.html\"\n    }\n  }\n}", content_type: :json)
+    end
   end
 
   private
+
+    def datacite_resource(identifier_uri)
+      RestClient::Resource.new identifier_uri, ENV["SCHOLAR_DOI_USERNAME"], ENV["SCHOLAR_DOI_PASSWORD"]
+    end
 
     def post_uri(identifier_uri)
       u = URI.parse(identifier_uri)
@@ -16,5 +26,10 @@ class IdentifierDeleteJob < ActiveJob::Base
 
     def doi_remote_service
       @doi_remote_service ||= Hydra::RemoteIdentifier.remote_service(:doi)
+    end
+
+    def get_state(identifier_uri)
+      response = JSON.parse(datacite_resource(identifier_uri).get)
+      response["data"]["attributes"]["state"]
     end
 end
