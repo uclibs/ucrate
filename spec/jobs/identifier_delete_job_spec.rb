2ae6272d926bab6d3574ffc39471cd576f9c321b
Chuck Greenman <charlesgreenman@gmail.com>
2019-05-24 11:34:21 -0400

    Modify VCR cassets.

--- spec/jobs/identifier_delete_job_spec.rb
+++ spec/jobs/identifier_delete_job_spec.rb
@@ -8,7 +8,7 @@ describe IdentifierDeleteJob do
   end
 
   let(:identifier_uri) { "https://api.test.datacite.org/dois/10.23676/2j0v-ft05" }
-  let(:expected_post_uri) { "https://CIN.TEST:ox6iQPX2tWcveNoUfM3qdf82aoeP@api.test.datacite.org/dois/10.23676/2j0v-ft05" }
+  let(:expected_post_uri) { "https://apitest:apitest@api.test.datacite.org/dois/10.23676/2j0v-ft05" }
 
   before do
     allow(Hydra::RemoteIdentifier).to receive(:remote_service).and_return(doi_remote_service)
@@ -16,13 +16,15 @@ describe IdentifierDeleteJob do
 
   describe "#perform" do
     it "calls RestClient with the correct params" do
-      expect(RestClient).to receive(:put).with(
-        expected_post_uri,
-        "{\n  \"data\": {\n    \"type\": \"dois\",\n    \"attributes\": {\n      \"event\": \"hide\",\n   \"url\": \"https://datacite.org/invalid.html\"\n    }\n  }\n}",
-        content_type: :json
-      )
+      VCR.use_cassette "remotely_identified_doi_delete_work", record: :new_episodes do
+        expect(RestClient).to receive(:put).with(
+          expected_post_uri,
+          "{\n  \"data\": {\n    \"type\": \"dois\",\n    \"attributes\": {\n      \"event\": \"hide\",\n   \"url\": \"https://datacite.org/invalid.html\"\n    }\n  }\n}",
+          content_type: :json
+        )
 
-      described_class.perform_now(identifier_uri)
+        described_class.perform_now(identifier_uri)
+      end
     end
   end
 end
