5f9a3239f4bf2099e21b06a25cbc48c1433ee87c
Uzval-Mallepeddi <uzval007@gmail.com>
2020-04-17 18:00:56 -0400

    Highlight Search Terms

---
+++ app/helpers/mark_helper.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module MarkHelper
+  def catalog(input_text)
+    value = input_text.to_s
+    filter_chars = params[:q].to_s.split(' ')
+    if filter_chars.empty?
+      filter_chars.each do |char|
+        value = value.gsub!(/#{char}/i, '<mark>' + char + '</mark>').to_s
+        value = input_text.to_s if value.empty?
+      end
+    else
+      value = value.to_s
+    end
+
+    value.html_safe
+  end
+end
