2e36b547ef7f1423dc990474fce6f470925709ce
Glen Horton <hortongn@ucmail.uc.edu>
2020-07-15 11:00:17 -0400

    Change coveralls service to Circle-CI

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,16 @@ def ci_build?
   ENV['TRAVIS'] || ENV['CIRCLE']
 end
 
+require 'simplecov'
+require 'coveralls'
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
+  [
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+  ]
+)
+SimpleCov.start('rails')
+
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
