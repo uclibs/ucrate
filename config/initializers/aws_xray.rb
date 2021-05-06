c8039d54f401f1543b269aad5ca74b8546ae027c
bhanutejags <bhanutejags@gmail.com>
2021-05-06 14:11:22 -0500

    Added dedicated log file for XRay. (#923)

--- config/initializers/aws_xray.rb
+++ config/initializers/aws_xray.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 Rails.application.config.xray = {
-  name: 'Scholar@uc',
+  name: "Scholar-#{Rails.env}@uc",
   patch: %I[net_http aws_sdk],
   # record db transactions as subsegments
   active_record: true,
-  context_missing: 'LOG_ERROR'
+  context_missing: 'LOG_ERROR',
+  logger: Logger.new("log/#{Rails.env}-xray.log")
 }
