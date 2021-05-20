a4ef397722ca755ff38bf95459fe8673a8a4f4be
bhanutejags <bhanutejags@gmail.com>
2021-05-20 15:01:17 -0500

    Added XRay Segment and log statements to Sidekiq jobs.

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,4 +1,28 @@
 # frozen_string_literal: true
 
+require 'aws-xray-sdk'
+
 class ApplicationJob < ActiveJob::Base
+
+  after_enqueue do |job|
+    Rails.logger.info "#{Time.current}: Enqueued job: #{self.class.name}: #{job.inspect}"
+  end
+
+  around_perform do |job, block|
+    Rails.logger.info "#{Time.current}: Beginning execution of the job: #{job.inspect}"
+    # Create XRay Segment before perform
+    subsegment = XRay.recorder.begin_segment("sidekiq::#{self.class.name}")
+    subsegment.annotations.update context: 'sidekiq'
+    subsegment.annotations.update job_id: job.job_id
+    subsegment.annotations.update provider_job_id: job.provider_job_id
+    job.attributes.each { |k,v|
+      subsegment.metadata.update "job_info_#{k}": v
+    }
+    subsegment.metadata.update job_info: job.inspect
+    # yield
+    block.call
+    # Destroy XRay Segment after perform
+    XRay.recorder.end_segment
+    Rails.logger.info "#{Time.current}: finished execution of the job: #{job.inspect}"
+  end
 end
