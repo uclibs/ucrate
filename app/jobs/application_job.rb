2c56751323bfda256891818b4f269193c1018138
bhanutejags <bhanutejags@gmail.com>
2021-05-21 18:56:51 -0500

    Added XRay Segment and log statements to Sidekiq jobs.

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -4,20 +4,34 @@ require 'aws-xray-sdk'
 
 class ApplicationJob < ActiveJob::Base
 
-  after_enqueue do |job|
+  def serialize
+    super.merge('parent_trace_id' => @parent_trace_id || XRay.recorder.current_segment.trace_id)
+  end
+
+  def deserialize(job_data)
+    super(job_data)
+    @parent_trace_id = job_data['parent_trace_id']
+  end
+
+  around_enqueue do |job, block|
+    Rails.logger.info "#{Time.current}: Enqueuing job: #{self.class.name}: #{job.inspect}"
+    XRay.recorder.capture("job_enqueue::#{self.class.name}") do |subsegment|
+      subsegment.metadata.update job_info: job.inspect
+      subsegment.annotations.update job_id: job.job_id
+      subsegment.annotations.update provider_job_id: job.provider_job_id
+      block.call
+    end
     Rails.logger.info "#{Time.current}: Enqueued job: #{self.class.name}: #{job.inspect}"
   end
 
   around_perform do |job, block|
     Rails.logger.info "#{Time.current}: Beginning execution of the job: #{job.inspect}"
     # Create XRay Segment before perform
-    subsegment = XRay.recorder.begin_segment("sidekiq::#{self.class.name}")
+    # subsegment = XRay.recorder.begin_segment("sidekiq::#{self.class.name}")
+    subsegment = XRay.recorder.begin_segment("sidekiq::#{self.class.name}", parent_id: @parent_trace_id || nil )
     subsegment.annotations.update context: 'sidekiq'
     subsegment.annotations.update job_id: job.job_id
     subsegment.annotations.update provider_job_id: job.provider_job_id
-    job.attributes.each { |k,v|
-      subsegment.metadata.update "job_info_#{k}": v
-    }
     subsegment.metadata.update job_info: job.inspect
     # yield
     block.call
