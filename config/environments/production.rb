2614be6123f1872ab969956f250f3de85b50a61f
bhanutejags <bhanutejags@gmail.com>
2021-05-26 14:31:32 -0500

    Changing the default production log level to `debug`.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
+  config.log_level = :info
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
