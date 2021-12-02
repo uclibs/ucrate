c293092677bc065cb3e837390a5f99c19c9d89d4
crowesn <crowesn@gmail.com>
2021-12-02 13:31:04 -0500

    escape output from mark_helper

--- spec/features/catalog_facet_spec.rb
+++ spec/features/catalog_facet_spec.rb
@@ -26,8 +26,8 @@ RSpec.describe 'catalog searching', js: true, type: :feature do
         click_button('Go')
       end
       expect(page).to have_content('Search Results')
-      expect(page).to have_content(jills_work.title.first)
-      expect(page).to have_content(jacks_work.title.first)
+      expect(page).to have_content("Jill's <mark>Research</mark>")
+      expect(page).to have_content("Jack's <mark>Research</mark>")
       expect(page).to have_selector('.facet-field-heading', text: 'Type of Work')
       expect(page).to have_selector('.facet-field-heading', text: 'Language')
       expect(page).to have_selector('.facet-field-heading', text: 'Publisher')
