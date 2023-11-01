4b96381e1f3657753e812008a6a03c4d4e1a7dbc
crowesn <crowesn@gmail.com>
2023-11-01 09:54:54 -0400

    add interactive verification check to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -65,3 +65,26 @@ task :start_prod do
     execute "cd #{fetch(:deploy_to)}/current/script && chmod a+x * && source start_prod.sh"
   end
 end
+
+namespace :deploy do
+  task :confirmation do
+    stage = fetch(:stage).upcase
+    branch = fetch(:branch)
+    puts <<-WARN
+     ========================================================================
+       *** Deploying branch `#{branch}` to #{stage} server ***
+       WARNING: You're about to perform actions on #{stage} server(s)
+       Please confirm that all your intentions are kind and friendly
+     ========================================================================
+     WARN
+    ask :value, "Sure you want to continue deploying `#{branch}` on #{stage}? (Y)"
+    unless fetch(:value).match?(/\A(?i:yes|y)\z/)
+      puts "\nNo confirmation - deploy cancelled!"
+      exit
+    end
+  end
+end
+
+Capistrano::DSL.stages.each do |stage|
+  after stage, 'deploy:confirmation'
+end
