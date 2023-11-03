6fdc4142b26b3c38fd97f45116c9d07aaf081d1c
Sean Crowe <1069588+crowesn@users.noreply.github.com>
2023-11-03 12:34:37 -0400

    add interactive verification check to deploy (#1093)
    Co-authored-by: crowesn <crowesn@gmail.com>

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -11,6 +11,11 @@ set :bundle_path, -> { shared_path.join('vendor/bundle') }
 append :linked_dirs, "tmp", "public"
 ask(:username, nil)
 ask(:password, nil, echo: false)
+ask(:value, 'Have you submitted and received an approved Change Management Request? (Y)')
+unless fetch(:value).match?(/\A(?i:yes|y)\z/)
+  puts "\nNo confirmation - deploy cancelled!"
+  exit
+end
 # NOTE: Removing :db from one of the servers makes the migrations run only once between thems
 server "localhost", user: fetch(:username), password: fetch(:password), port: 2222, roles: [:web, :app, :db]
 server "localhost", user: fetch(:username), password: fetch(:password), port: 2223, roles: [:web, :app]
