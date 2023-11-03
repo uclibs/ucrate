1694429d25db51e4c7161c5c16cd2bd98e281a7e
Janell-Huyck <huyckjl@ucmail.uc.edu>
2023-11-03 12:51:00 -0400

    restored documents_controller_spec to original version

---
+++ spec/controllers/hyrax/documents_controller_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# Generated via
+#  `rails generate hyrax:work Document`
+require 'rails_helper'
+
+RSpec.describe Hyrax::DocumentsController do
+  it "has tests" do
+    skip "Add your tests here"
+  end
+end
