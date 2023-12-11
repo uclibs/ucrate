4318e8ca4d05f63f34ec3f4d49d6afa77a6f1213
Thomas Scherz <scherztc@ucmail.uc.edu>
2023-12-11 14:06:03 -0500

    Gets the rspec test running.

--- app/indexers/hyrax/collection_indexer.rb
+++ app/indexers/hyrax/collection_indexer.rb
@@ -2,21 +2,23 @@
 module Hyrax
   class CollectionIndexer < Hydra::PCDM::CollectionIndexer
     include Hyrax::IndexesThumbnails
-    include SortableTitleIndexer
+#    include SortableTitleIndexer
 
-    STORED_LONG = Solrizer::Descriptor.new(:long, :stored)
-
+#    STORED_LONG = Solrizer::Descriptor.new(:long, :stored)
+    STORED_LONG = ActiveFedora::Indexing::Descriptor.new(:long, :stored)
     self.thumbnail_path_service = Hyrax::CollectionThumbnailPathService
 
     def generate_solr_document
       super.tap do |solr_doc|
         # Makes Collections show under the "Collections" tab
-        Solrizer.set_field(solr_doc, 'generic_type', 'Collection', :facetable)
+        solr_doc['generic_type_sim'] = ["Collection"]
+        solr_doc['visibility_ssi'] = object.visibility
+        solr_doc['thumbnail_path_ss'] = thumbnail_path
+ 
+        # Solrizer.set_field(solr_doc, 'generic_type', 'Collection', :facetable)
         # Index the size of the collection in bytes
         solr_doc[Solrizer.solr_name(:bytes, STORED_LONG)] = object.bytes
-        solr_doc['thumbnail_path_ss'] = thumbnail_path
-        solr_doc['visibility_ssi'] = object.visibility
-        Solrizer.insert_field(solr_doc, 'sort_title', sortable_title(object.title.first), :stored_sortable) if object.title.present?
+        # Solrizer.insert_field(solr_doc, 'sort_title', sortable_title(object.title.first), :stored_sortable) if object.title.present?
 
         object.in_collections.each do |col|
           (solr_doc['member_of_collection_ids_ssim'] ||= []) << col.id
