1245f8352859271e7208466b2328ba868884da08
Thomas Scherz <scherztc@ucmail.uc.edu>
2024-04-06 15:34:48 -0400

    Includes Devise::Test::ControllerHelpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -147,6 +147,7 @@ RSpec.configure do |config|
   config.include Devise::Test::IntegrationHelpers, type: :feature
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :helper
+  #  config.include Devise::Test::ControllerHelpers
 
   config.before :suite do
     DatabaseCleaner.clean_with(:truncation)
