b6b1d838f61fe94cc1f151c0b336c8bc40aff9cb
Thomas Scherz <scherztc@ucmail.uc.edu>
2024-05-13 13:56:10 -0400

    Use ENVIRONMENT variable in embargo scripts.

--- script/embargo_notify.sh
+++ script/embargo_notify.sh
@@ -1,3 +1,29 @@
-cd /srv/apps/curate_uc
-export PATH=$PATH:/srv/apps/.gem/ruby/2.7.0/bin
-RAILS_ENV=production bundle exec rake embargo_notify
+#!/bin/sh
+
+# Notify editors that a work has been released
+# script/embargo_notify.sh [production|development]
+
+ENVIRONMENT=$1
+
+APP_DIRECTORY="$(dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" )"
+
+
+if [ $# -eq 0 ]; then
+    echo -e "ERROR: no environment argument [production|development] provided"
+    exit 1
+fi
+
+if [ $ENVIRONMENT != "production" ] && [ $ENVIRONMENT != "development" ]; then
+    echo -e "ERROR: environment argument must be either [production|development] most likely this will be development for local machines and production otherwise"
+    exit 1
+fi
+
+if [[ $ENVIRONMENT == "production" ]]; then
+    export PATH=$PATH:/srv/apps/.gem/ruby/2.7.0/bin
+fi
+
+banner "Notify Embargoe Editors"
+
+cd $APP_DIRECTORY
+
+RAILS_ENV=$ENVIRONMENT bundle exec rake embargo_notify
--- script/release_embargo.sh
+++ script/release_embargo.sh
@@ -1,9 +1,27 @@
-#!/bin/bash
+#!/bin/sh
 
 # Change and expired embargoed objects to open access
 # Runs as a cron job daily just after midnight
+# script/release_embargo.sh [production|development]
 
-cd /srv/apps/curate_uc
-export PATH=$PATH:/srv/apps/.gem/ruby/2.7.0/bin
+ENVIRONMENT=$1
+
+APP_DIRECTORY="$(dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" )"
+
+if [ $# -eq 0 ]; then
+    echo -e "ERROR: no environment argument [production|development] provided"
+    exit 1
+fi
+
+if [ $ENVIRONMENT != "production" ] && [ $ENVIRONMENT != "development" ]; then
+    echo -e "ERROR: environment argument must be either [production|development] most likely this will be development for local machines and production otherwise"
+    exit 1
+fi
+
+if [[ $ENVIRONMENT == "production" ]]; then
+    export PATH=$PATH:/srv/apps/.gem/ruby/2.7.0/bin
+fi
+
+cd $APP_DIRECTORY
 export RELEASE_DATE=`date +\%Y-\%m-\%d -d "+1 day"`
-RAILS_ENV=production bundle exec rake embargo_release["$RELEASE_DATE"]
+RAILS_ENV=$ENVIRONMENT bundle exec rake embargo_release["$RELEASE_DATE"]
