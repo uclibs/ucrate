7f74b32ff842e1245255cf82fbf5e5b8eaec20c1
Thomas Scherz <thomas.scherz@uc.edu>
2025-01-27 14:26:19 -0500

    Conditionalizes delete temp file script. (#1171)
    Co-authored-by: Thomas Scherz <scherztc@ucmail.uc.edu>

--- script/delete_old_temp_uploads.sh
+++ script/delete_old_temp_uploads.sh
@@ -3,4 +3,9 @@
 UPLOADS_DIRECTORY=/mnt/common/scholar-temp-uploads/hyrax/uploaded_file/file
 MAXIMUM_DAYS_TO_KEEP_FILES=30
 
-find $UPLOADS_DIRECTORY/* -maxdepth 0 -type d -ctime +$MAXIMUM_DAYS_TO_KEEP_FILES -exec rm -rf {} \;
+# Check if the directory exists and is not empty
+if [ -d "$UPLOADS_DIRECTORY" ] && [ "$(ls -A "$UPLOADS_DIRECTORY")" ]; then
+    find "$UPLOADS_DIRECTORY"/* -maxdepth 0 -type d -ctime +$MAXIMUM_DAYS_TO_KEEP_FILES -exec rm -rf {} \;
+else
+    echo "Directory $UPLOADS_DIRECTORY is either non-existent or empty. Skipping cleanup."
+fi
