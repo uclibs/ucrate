94b0cd2bb384ac6dda2b96881c10f205d1ea7bdb
Thomas Scherz <scherztc@ucmail.uc.edu>
2025-03-04 14:46:45 -0500

    Removes Willow Sword gem, updates Redis and Sidekiq.

--- config/initializers/redis_config.rb
+++ config/initializers/redis_config.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 require 'redis'
+
 config = YAML.safe_load(ERB.new(IO.read(Rails.root.join('config', 'redis.yml'))).result)[Rails.env].with_indifferent_access
-Redis.current = Redis.new(config.merge(thread_safe: true))
+
+# Define a global Redis instance
+$redis = Redis.new(url: config[:url])
