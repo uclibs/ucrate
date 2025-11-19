863566358c574dbe81b8b5e3b0f319d19f381dd3
Janell Huyck <huyckjl@ucmail.uc.edu>
2025-11-19 13:33:24 -0500

    Fix Coveralls coverage reporting by removing double SimpleCov initialization
    - Remove SimpleCov/Coveralls setup from spec_helper.rb to prevent double initialization
    - Consolidate all coverage setup in rails_helper.rb
    - Add simplecov-lcov and configure LCOV formatter for CircleCI
    - Use SimpleCov.formatters (plural) with all three formatters: HTML, LCOV, and Coveralls
    - Configure formatters before starting SimpleCov to ensure proper setup

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,10 +9,16 @@ def ci_build?
 end
 
 require 'simplecov'
+require 'simplecov-lcov'
 require 'coveralls'
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
+
+SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+
+# Configure formatters before starting SimpleCov
+SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new(
   [
     SimpleCov::Formatter::HTMLFormatter,
+    SimpleCov::Formatter::LcovFormatter,
     Coveralls::SimpleCov::Formatter
   ]
 )
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,28 +16,6 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
-require 'simplecov'
-require 'simplecov-lcov'
-require 'coveralls'
-
-SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
-SimpleCov.start 'rails'
-
-SimpleCov.at_exit do
-  SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
-  SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
-  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
-    [
-      SimpleCov::Formatter::HTMLFormatter,
-      SimpleCov::Formatter::LcovFormatter,
-      Coveralls::SimpleCov::Formatter
-    ]
-  )
-  SimpleCov.result.format!
-end
-
-Coveralls.wear!('rails')
-
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
