6b1e50c32f907b341f96e9af87278452bb8e13d6
Janell Huyck <57018862+Janell-Huyck@users.noreply.github.com>
2026-07-20 12:02:57 -0400

    Use good_job in CI and save site themes before redirect.
    Account#find_job was hitting Sidekiq Redis with thread_safe: true and raising
    ArgumentError; Hyku Docker uses HYRAX_ACTIVE_JOB_QUEUE=good_job. Theme form
    updates now run before the success redirect so Site theme attributes persist.

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -7,7 +7,13 @@ class SitesController < ApplicationController
 
   def update
     # FIXME: Pull these strings out to i18n locale
-    if @site.update(update_params)
+    # Apply theme params before redirect. Updating after redirect_to still runs
+    # in MRI Rails, but success was announced even when themes were never saved
+    # (e.g. empty update_params path). Theme form posts to this action via
+    # site_path; image-removal buttons also post here.
+    themes_ok = params[:site] ? @site.update(site_theme_params) : true
+
+    if themes_ok && @site.update(update_params)
 
       # If updating work or collection default image, works and/or collections should be reindexed
       # as they are when the default image is added. See AppearancesControllerDecorator#update
@@ -26,8 +32,6 @@ class SitesController < ApplicationController
     else
       redirect_to hyrax.admin_appearance_path, flash: { error: 'Updating the appearance was unsuccessful.' }
     end
-
-    @site.update(site_theme_params) if params[:site]
   end
 
   private
